77cbc63c52e47fdd17e62decdad94c6edb13a9ff
Cristian Hernández <cristian.camilo1560@gmail.com>
2022-10-29 10:50:51 -0500

    Se obtuvieron las caracteristicas de los personajes

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import tiposdepokemon from '../../clases/tiposdepokemon';
+import './style.css'
+
+function Card({ pokemon }) {
+  return (
+      <div className="Card">
+          <div className="Card_img">
+              <img src={pokemon.sprites.front_default} alt="" />
+          </div>
+          <div className="Card_name">
+              {pokemon.name}
+          </div>
+          <div className="Card_types">
+              {
+                  pokemon.types.map(type => {
+                      return (
+                          <div className="Card_type" style={{ backgroundColor: tiposdepokemon[type.type.name] }}>
+                              {type.type.name}
+                          </div>
+                      )
+                  })
+              }
+          </div>
+          <div className="Card_info">
+              <div className="Card_data Card_data--weight">
+                  <p className="title">Peso</p>
+                  <p>{pokemon.weight}</p>
+              </div>
+              <div className="Card_data Card_data--weight">
+                  <p className="title">Altura</p>
+                  <p>{pokemon.height}</p>
+              </div>
+              <div className="Card_data Card_data--ability">
+                  <p className="title">Habilidad</p>
+                  <p>{pokemon.abilities[0].ability.name}</p>
+              </div>
+          </div>
+      </div>
+  );
+}
+
+export default Card;
